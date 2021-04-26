836901db1a7540148968375e35e968740d289b16
Dylan Ellsworth <dellsworth39@gmail.com>
2021-04-26 03:25:30 -0700

    adding ruby files

--- html/cgi-bin/ruby-hello-html-world.rb
+++ html/cgi-bin/ruby-hello-html-world.rb
@@ -1,4 +1,23 @@
-#!/usr/bin/env ruby
-print "HTTP/1.0 200 OK\r\n"
-print "Content-type: text/html\r\n\r\n"
-print "<html><body>Hello World!</body></html>\r\n"
+#!/usr/local/bin/ruby
+require 'cgi'
+print "Cache-Control: no-cache\n";
+print "Content-type: text/html\n\n";
+print "<html>";
+print "<head>";
+print "<title>Hello, Perl!</title>";
+print "</head>";
+print "<body>";
+
+print "<h1>Hackinthebox was here - Hello, Ruby!</h1>";
+print "<p>This page was generated with the Ruby  programming langauge</p>";
+
+date = Time.new;
+print "<p>Current Time: #{date}</p>";
+
+# IP Address is an environment variable when using CGI
+address = ENV['REMOTE_ADDR'];
+print "<p>Your IP Address: #{address}</p>";
+
+print "</body>";
+print "</html>";
+print "<html><body>Hello World!</body></html>\r\n";
