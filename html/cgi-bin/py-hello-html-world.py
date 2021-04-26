b06781d2f7bba65b3acc52a84ebbd82f8e14b582
bchangucsd <60710177+bchangucsd@users.noreply.github.com>
2021-04-25 20:40:17 -0700

    started part1 python

---
+++ html/cgi-bin/py-hello-html-world.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+#import cgitb
+#cgitb.enable()
+
+import cgitb
+cgitb.enable()
+
+print("Content-Type: text/html")    # HTML is following
+print()                             # blank line, end of headers
+print("Hello")
