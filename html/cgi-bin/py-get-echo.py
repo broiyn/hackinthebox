bd486c3a0b1f1c32e497fd4724aa5c74a693a3ee
bchangucsd <60710177+bchangucsd@users.noreply.github.com>
2021-04-26 22:11:37 -0700

    created py get echo

---
+++ html/cgi-bin/py-get-echo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import cgitb
+import os
+
+cgitb.enable()
+
+print("Cache-Control: no-cache")
+print("Content-type: text/html")
+print()
+
+print("""
+<!DOCTYPE html>
+<html><head><title>GET Request Echo</title>
+</head><body><h1 align="center">Get Request Echo</h1>
+<hr>
+""")
+
+print("b>Query String:</b> %s<br />" % (os.environ['QUERY_STRING']))
+
+
+
+print("</body></html>")
