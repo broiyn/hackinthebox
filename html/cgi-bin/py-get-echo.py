c8ada88faed1562c840dc9e7375957a4e9936c02
bchangucsd <60710177+bchangucsd@users.noreply.github.com>
2021-04-26 22:24:15 -0700

    py get echo

--- html/cgi-bin/py-get-echo.py
+++ html/cgi-bin/py-get-echo.py
@@ -18,6 +18,13 @@ print("""
 
 print("<b>Query String:</b> %s<br />" % (os.environ['QUERY_STRING']))
 
-
+if len(os.environ['QUERY_STRING']) > 0:
+  buffer = os.environ['QUERY_STRING']
+  pairs = buffer.split('&')
+  print("<table><b>Formatted Query String:</b>")
+  for pair in pairs:
+    name = pair.split('=')
+    print("<tr><td>%s:</td><td>%s</td></tr>" % (name[0],name[1]))
+  print("</table>")
 
 print("</body></html>")
