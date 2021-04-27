2971b45a62ab07ac327416696162f8f133800ec0
bchangucsd <60710177+bchangucsd@users.noreply.github.com>
2021-04-26 19:28:26 -0700

    python hello world

--- html/cgi-bin/py-hello-html-world.py
+++ html/cgi-bin/py-hello-html-world.py
@@ -3,8 +3,27 @@
 #cgitb.enable()
 
 import cgitb
+import time
+import os
+
 cgitb.enable()
 
 print("Content-Type: text/html")    # HTML is following
+print("Content-type: text/html\n\n")
 print()                             # blank line, end of headers
-print("Hello")
+
+print("<html>")
+print("<head>")
+print("<title>Hello, Python!</title>")
+print("</head>")
+print("<body>")
+print("<h1 align=center>Hello Python!</h1>")
+print("<hr>")
+localtime = time.asctime( time.localtime(time.time()) )
+print("<p>This page was generated with the Python programming langauge</p>")
+print("<p>Current Time:", localtime, "</p>")
+
+print("<p>Your IP Address:", os.environ["REMOTE_ADDR"],"</p>")
+
+print("</body>")
+print("</html>")
