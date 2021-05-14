96b2d6377e33e4dfcb87244d401e71cfdf3e0aec
nmn009 <nmn009@ucsd.edu>
2021-05-14 13:58:10 -0700

    update

--- html/collector.js
+++ html/collector.js
@@ -1,57 +1,124 @@
 // Collect Static here
-function collect_user_agent() {
-    var user = navigator.userAgent;         //
-}
 
-function collect_user_lang() {
-    var lang = navigator.language;          //language current used
-}
+var user = navigator.userAgent;
 
-function allow_cookies() {
-    var cookie = navigator.cookieEnabled;   //True if cookie is Enabled, False otherwise
-}
+var lang = navigator.language;          //language current used
 
-function allow_JS() {
+var cookie = navigator.cookieEnabled;   //True if cookie is Enabled, False otherwise
 
-}
+var JS_allowed = true;                  //need to find out how to implement this
 
-function allow_images() {
+var images_allowed = true;              //need to find out how to implement this
 
-}
+var CSS_allowed = true;
 
-function allow_CSS() {
-    
-}
+var height_screen = screen.height;
+var width_screen = screen.width;
 
-function screen_dimensions() {
-    var height_screen = screen.height;
-    var width_screen = screen.width;
-}
+var height_window = window.innerHeight;
+var width_window = window.innerWidth;
 
-function window_dimensions() {
-    var height_window = window.innerHeight;
-    var width_window = window.innerWidth;
-}
+var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+var type = connection.effectiveType;
 
-function network_connection() {
-    var connection = navigator.connection || navigator.mozConnection || navigator.webkitConnection;
-    var type = connection.effectiveType;
-}
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // Collect Perormance here
-function get_page_load(){
 
-    //object loading time
 
-    //time when page start loading
-    var start_timer = Date.now();
-    //time when page done loading
-    
+//object loading time
 
-    //time when the page completely loaded
-}
+//time when page start loading
+var start_timer = Date.now();
+//time when page done loading
+
+
+//time when the page completely loaded
+
 
 
 // Collect Activity
+
+
+
+//this function will automatic launch every time we load the page
+function fetch_data() {
+    // Fetch the collected data here
+    postdata = {
+        'user_agent_string': user,
+        'user_language': lang,
+        'cookies_is_allowed': cookie,
+        'JS_allowed': JS_allowed,
+        'images_allowed': images_allowed,
+        'CSS_allowed': CSS_allowed,
+        'screen_height': height_screen,
+        'screen_width': width_screen,
+        'window_height': height_window,
+        'window_width': width_window,
+        'network_connection': type
+    };
+    fetch("https://hackinthebox.site/json/posts", { // note we are going to /1
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postdata)
+    });
+}
+
+function fetch_activity() {
+    postdata = {
+        'user_agent_string': user,
+        'user_language': lang,
+        'cookies_is_allowed': cookie,
+        'JS_allowed': JS_allowed,
+        'images_allowed': images_allowed,
+        'CSS_allowed': CSS_allowed,
+        'screen_height': height_screen,
+        'screen_width': width_screen,
+        'window_height': height_window,
+        'window_width': width_window,
+        'network_connection': type
+    };
+    fetch("https://hackinthebox.site/json/user", { // note we are going to /1
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postdata)
+    });
+
+    fetch("https://hackinthebox.site/json/user", { // note we are going to /1
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(postdata)
+});
+}
+
+function fetch_performance() {
+    postdata = {
+        'user_agent_string': user,
+        'user_language': lang,
+        'cookies_is_allowed': cookie,
+        'JS_allowed': JS_allowed,
+        'images_allowed': images_allowed,
+        'CSS_allowed': CSS_allowed,
+        'screen_height': height_screen,
+        'screen_width': width_screen,
+        'window_height': height_window,
+        'window_width': width_window,
+        'network_connection': type
+    };
+    fetch("https://hackinthebox.site/json/performance", { // note we are going to /1
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postdata)
+    });
+
+
+}
