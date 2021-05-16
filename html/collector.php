29fbaa09258c621d9092a1172f24efc56fba0df8
bchangucsd <60710177+bchangucsd@users.noreply.github.com>
2021-05-15 23:59:04 -0700

    js enabled

---
+++ html/collector.php
@@ -0,0 +1,21 @@
+<?php
+  // Create some random text-encoded data for a line chart.
+  header('content-type: application/json');
+  
+  // Add data, chart type, chart size, and scale to params.
+  $data = array('JS_allowed' => 'false',);
+
+  // Send the request, and print out the returned bytes.
+  $context = stream_context_create(
+    array('http' => array(
+      'header'  => "Content-type: application/json\r\n",
+      'method' => 'POST',
+      'content' => http_build_query($data)
+    ))
+  );
+
+  $fp = fopen('https://hackinthebox.site/json/posts/', 'r', false, $context);
+  fpassthru($fp);
+  fclose($fp);
+
+?>
